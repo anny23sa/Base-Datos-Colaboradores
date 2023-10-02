0f539b057a2557e0b4d5500314854f95fb9f02c1
yoskary80@gmail.com <yoskary80@gmail.com>
2023-10-01 21:24:25 -0300

    se agrega buscador

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Buscador = () => {
+const Search = ({ dataBuscador }) => {
+  const handleBuscador = (e) => {
+    dataBuscador(e.target.value);
+  };
+
   return (
-    <div>Buscador</div>
-  )
-}
+    <>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand">Listado de colaboradores</a>
+          <form className="d-flex" role="buscar">
+            <input
+              className="form-control me-2"
+              type="buscar"
+              placeholder="Busqueda"
+              aria-label="Buscar"
+              onChange={handleBuscador}
+            />
+          </form>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Buscador
+export default Search;
