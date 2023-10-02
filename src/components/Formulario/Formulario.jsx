7c43803b66d9560de845ff445379e10a595c5077
Anny Stecca <anny23sa@gmail.com>
2023-10-01 20:34:02 -0700

    cambio en formulario

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div>
+        <h1>Base de Datos colaboradores</h1>
+        <label className="form-label" htmlFor="name">Nombre</label>
+        <input className="form-control" type="text" name="name" id="name" />
+    </div>
   )
 }
 
