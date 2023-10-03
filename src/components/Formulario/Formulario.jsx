312f4b78578ca2c89456beaf26f24b45dff1a619
Anny Stecca <anny23sa@gmail.com>
2023-10-02 18:52:46 -0700

    se crea Formulario.css

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,115 +1,118 @@
-import React from 'react'
+//import React from 'react'
 //import { Alert } from 'bootstrap'
 //import React from 'react'
 import { useState } from 'react'
+import './Formulario.css';
 
 
-const Formulario = ({agregarColaborador}) => {
-    const [nombre, setNombre] = useState('')
-    const [email, setEmail] = useState('')
-    const [edad, setEdad] = useState('')
-    const [cargo, setCargo] = useState('')
-    const [telefono, setTelefono] = useState('')
+const Formulario = ({ agregarColaborador }) => {
+  const [nombre, setNombre] = useState('')
+  const [email, setEmail] = useState('')
+  const [edad, setEdad] = useState('')
+  const [cargo, setCargo] = useState('')
+  const [telefono, setTelefono] = useState('')
 
-    const [error, setError] =useState(false)
-    const [mensaje, setMensaje] = useState('')
+  const [error, setError] = useState(false)
+  const [mensaje, setMensaje] = useState('')
 
-    const validarDatos =(e) => {
-      e.preventDefault();
+  const validarDatos = (e) => {
+    e.preventDefault();
 
-     if (nombre === ''|| email === ''|| edad ===''||cargo === ''|| telefono === '' ){
+    if (nombre === '' || email === '' || edad === '' || cargo === '' || telefono === '') {
       setError(true);
       setMensaje('Completa todos los campos.');
-     } 
-     //resstablecer errores si los datos son validos
-     setError(false);
-     setMensaje('');
-     };
-
-     const nuevoColaborador ={
-      id: Date.now().toString(),
-      nombre,
-      email,
-      edad,
-      cargo,
-      telefono,
-     };
-     //agregar el nuevo colaborador a la lista
-     agregarColaborador(nuevoColaborador);
-// Limpiar los campos del formulario
-setNombre('');
-setEmail('');
-setEdad('');
-setCargo('');
-setTelefono('');
-
-// Reiniciar el estado de error a false
-setError(false);
-};
+    } else {
+      //resstablecer errores si los datos son validos
+      setError(false);
+      setMensaje('');
 
 
-    return (
-        <div className='container'>
-          <h2>Agregar colaborador</h2>
-          <form className="Formulario" onSubmit={validarDatos}>
-            {error && <p>{mensaje} </p>}
-            <div className="form-group">
-              <label></label>
-              <input type="text" 
-              placeholder='Nombre colaborador'
-              name="nombre" 
-              className="form-control w-50" 
-              value={nombre} 
-              onChange={(e) => setNombre(e.target.value)} />
-            </div>
+      const nuevoColaborador = {
+        id: Date.now().toString(),
+        nombre,
+        email,
+        edad,
+        cargo,
+        telefono,
+      };
+      //agregar el nuevo colaborador a la lista
+      agregarColaborador(nuevoColaborador);
+      // Limpiar los campos del formulario
+      setNombre('');
+      setEmail('');
+      setEdad('');
+      setCargo('');
+      setTelefono('');
 
-            <div className="form-group">
-              <label></label>
-              <input type="email"
-               placeholder='Email colaborador'
-              name="email" 
-              className="form-control w-50" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} />
-            </div>
+    }
+  };
+  // Reiniciar el estado de error a false
+  //setError(false);
+  //};
 
-            <div className="form-group">
-              <label></label>
-              <input type="text" 
-              placeholder='Edad colaborador'
-              name="edad" 
-              className="form-control w-50" 
-              value={edad} 
-              onChange={(e) => setEdad(e.target.value)} />
-            </div>
 
-            <div className="form-group">
-              <label></label>
-              <input type="text"
-               placeholder='Cargo Colaborador'
-              name="cargo" 
-              className="form-control w-50" 
-              value={cargo} 
-              onChange={(e) => setCargo(e.target.value)} />
-            </div>
-
-            <div className="form-group">
-              <label></label>
-              <input type="tel" 
-              placeholder='Teléfono colaborador'
-              name="telefono" 
-              className="form-control w-50" 
-              value={telefono} 
-              onChange={(e) => setCargo(e.target.value)} />
-            </div>
-
-            <br />
-            <button type="submit" className="btn btn-primary w-50">Agregar colaborador</button>
-          </form>
-          <br />
-
+  return (
+    <div className='container'>
+      <h2>Agregar colaborador</h2>
+      <form className="Formulario" onSubmit={validarDatos}>
+        {error && <p>{mensaje} </p>}
+        <div className="form-group">
+          <label></label>
+          <input type="text"
+            placeholder='Nombre colaborador'
+            name="nombre"
+            className="form-control w-50"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)} />
         </div>
-      );
 
+        <div className="form-group">
+          <label></label>
+          <input type="email"
+            placeholder='Email colaborador'
+            name="email"
+            className="form-control w-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <label></label>
+          <input type="text"
+            placeholder='Edad colaborador'
+            name="edad"
+            className="form-control w-50"
+            value={edad}
+            onChange={(e) => setEdad(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <label></label>
+          <input type="text"
+            placeholder='Cargo Colaborador'
+            name="cargo"
+            className="form-control w-50"
+            value={cargo}
+            onChange={(e) => setCargo(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <label></label>
+          <input type="tel"
+            placeholder='Teléfono colaborador'
+            name="telefono"
+            className="form-control w-50"
+            value={telefono}
+            onChange={(e) => setTelefono(e.target.value)} />
+        </div>
+
+        <br />
+        <button type="submit" className="btn btn-primary w-50">Agregar colaborador</button>
+      </form>
+      <br />
+
+    </div>
+  );
+};
 
 export default Formulario
