7daced8ae9248f6bdb6f6db6e2be097ad07efc45
Anny Stecca <anny23sa@gmail.com>
2023-10-03 11:05:57 -0700

    form

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -25,89 +25,89 @@ const Formulario = ({ agregarColaborador }) => {
       setMensaje('');
 
 
-      const nuevoColaborador = {
-        id: Date.now().toString(),
-        nombre,
-        email,
-        edad,
-        cargo,
-        telefono,
-      };
-      //agregar el nuevo colaborador a la lista
-      agregarColaborador(nuevoColaborador);
-      // Limpiar los campos del formulario
-      setNombre('');
-      setEmail('');
-      setEdad('');
-      setCargo('');
-      setTelefono('');
+    const nuevoColaborador = {
+      id: Date.now().toString(),
+      nombre,
+      email,
+      edad,
+      cargo,
+      telefono,
+    };
+    //agregar el nuevo colaborador a la lista
+    agregarColaborador(nuevoColaborador);
+    // Limpiar los campos del formulario
+    setNombre('');
+    setEmail('');
+    setEdad('');
+    setCargo('');
+    setTelefono('');
 
-    }
-  };
+  }
+};
 
 
-  return (
-    <div className='container'>
-      <h2>Agregar colaborador</h2>
-      <form className="Formulario" onSubmit={validarDatos}>
-        {error && <p class="pmensaje">{mensaje} </p>}
-        <div className="form-group">
-          <label></label>
-          <input type="text"
-            placeholder='Nombre colaborador'
-            name="nombre"
-            className="form-control w-50"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)} />
-        </div>
+return (
+  <div className='container'>
+    <h2>Agregar colaborador</h2>
+    <form className="Formulario" onSubmit={validarDatos}>
+      {error && <p class="pmensaje">{mensaje} </p>}
+      <div className="form-group">
+        <label></label>
+        <input type="text"
+          placeholder='Nombre colaborador'
+          name="nombre"
+          className="form-control w-50"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)} />
+      </div>
 
-        <div className="form-group">
-          <label></label>
-          <input type="email"
-            placeholder='Email colaborador'
-            name="email"
-            className="form-control w-50"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} />
-        </div>
+      <div className="form-group">
+        <label></label>
+        <input type="email"
+          placeholder='Email colaborador'
+          name="email"
+          className="form-control w-50"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
+      </div>
 
-        <div className="form-group">
-          <label></label>
-          <input type="text"
-            placeholder='Edad colaborador'
-            name="edad"
-            className="form-control w-50"
-            value={edad}
-            onChange={(e) => setEdad(e.target.value)} />
-        </div>
+      <div className="form-group">
+        <label></label>
+        <input type="text"
+          placeholder='Edad colaborador'
+          name="edad"
+          className="form-control w-50"
+          value={edad}
+          onChange={(e) => setEdad(e.target.value)} />
+      </div>
 
-        <div className="form-group">
-          <label></label>
-          <input type="text"
-            placeholder='Cargo Colaborador'
-            name="cargo"
-            className="form-control w-50"
-            value={cargo}
-            onChange={(e) => setCargo(e.target.value)} />
-        </div>
+      <div className="form-group">
+        <label></label>
+        <input type="text"
+          placeholder='Cargo Colaborador'
+          name="cargo"
+          className="form-control w-50"
+          value={cargo}
+          onChange={(e) => setCargo(e.target.value)} />
+      </div>
 
-        <div className="form-group">
-          <label></label>
-          <input type="tel"
-            placeholder='Teléfono colaborador'
-            name="telefono"
-            className="form-control w-50"
-            value={telefono}
-            onChange={(e) => setTelefono(e.target.value)} />
-        </div>
+      <div className="form-group">
+        <label></label>
+        <input type="tel"
+          placeholder='Teléfono colaborador'
+          name="telefono"
+          className="form-control w-50"
+          value={telefono}
+          onChange={(e) => setTelefono(e.target.value)} />
+      </div>
 
-        <br />
-        <button type="submit" className="btn btn-primary w-50">Agregar colaborador</button>
-      </form>
       <br />
+      <button type="submit" className="btn btn-primary w-50">Agregar colaborador</button>
+    </form>
+    <br />
 
-    </div>
-  );
+  </div>
+);
 };
 
 export default Formulario
