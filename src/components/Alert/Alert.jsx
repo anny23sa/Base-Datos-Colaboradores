f4cd13c3dceb8ae93af55fee6e83635a4e7fe401
Anny Stecca <anny23sa@gmail.com>
2023-10-03 19:51:08 -0700

    cambio mensaje alert

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -3,10 +3,10 @@
 import React from 'react';
 
 const Alert = ({ mensaje, tipo }) => {
-  const alertClass = tipo === 'success' ? 'alert-success' : 'alert-danger';
+  const alertClass = tipo === 'success' ? 'alert-success' : 'alert-danger' ;
 
   return (
-    <div className={`alert ${alertClass}`} role="alert">
+    <div className={`alert alert-${tipo} my-3`} role="alert">
       {mensaje}
     </div>
   );
